acfcd0cfccd24ad57610b42d6e5d1827363fe45b
Andy Yin <yin.andy32@gmail.com>
2023-03-07 19:10:10 +1100

    Added each route and css styling

--- app/javascript/controllers/direction_controller.js
+++ app/javascript/controllers/direction_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
 
     const map = new mapboxgl.Map({
-      container: this.element,
+      container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [144.947982, -37.818711], // starting position
       zoom: 15
@@ -35,7 +35,7 @@ export default class extends Controller {
       // an arbitrary start will always be the same
       // only the end or destination will change
       const query = await fetch(
-        `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
         { method: 'GET' }
       );
       const json = await query.json();
@@ -73,7 +73,14 @@ export default class extends Controller {
           }
         });
       }
-      // add turn instructions here at the end
+      const instructions = document.getElementById('instructions');
+      const steps = data.legs[0].steps;
+
+      let tripInstructions = '';
+      for (const step of steps) {
+        tripInstructions += `<li>${step.maneuver.instruction}</li>`;
+      }
+      instructions.innerHTML = `<p><strong>🚗 Trip duration: ${Math.floor(data.duration / 60)} min </strong></p> <ol>${tripInstructions}</ol>`;
     }
 
     map.on('load', () => {
